216e36e7b4f262a5d10afdfb366fbfa1480796df
Stepan Byhovtsov <st.byhkovtsoff@gmail.com>
2023-09-23 21:11:07 +0300

    Applied huge refactoring

---
+++ WebAuthMVC.Core/Abstractions/AbstractUser.cs
@@ -0,0 +1,35 @@
+﻿namespace WebAuth;
+
+public abstract class AbstractUser
+{
+   public abstract string Username { get; set; }
+   public abstract string PasswordHash { get; set; }
+
+   public bool VerifyPassword(string password, IPasswordEncryptionService encryptor)
+   {
+      return encryptor.Verify(PasswordHash, password);
+   }
+   
+   public bool VerifyHash(string passwordHash, IPasswordEncryptionService encryptor)
+   {
+      return ConstTimeHashEquals(PasswordHash, passwordHash);
+   }
+
+   public static bool ConstTimeHashEquals(string hashA, string hashB)
+   {
+      if (hashA.Length != hashB.Length)
+         throw new ArgumentException();
+
+      bool equal = true;
+      for (int i = 0; i < hashA.Length; i++)
+         if (hashA[i] != hashB[i])
+            equal = false;
+
+      return equal;
+   }
+
+   public static string HashPassword(string password, IPasswordEncryptionService encryptor)
+   {
+      return encryptor.GetHash(password);
+   }
+}
---
+++ WebAuthMVC.Infrastructure/Contexts/DbApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAuthMVC.Infrastructure.Entities;
+
+namespace WebAuthMVC.Models;
+
+public sealed class DbApplicationContext : DbContext
+{
+   public DbSet<User> Users => Set<User>();
+
+   public DbApplicationContext() => Database.EnsureCreated();
+   
+   protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+   {
+      optionsBuilder.UseSqlite("Data Source=users.db");
+   }
+}
